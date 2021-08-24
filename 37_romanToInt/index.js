871130ee4b53f30156afe2533be1dc5f40679138
Mohammad Sakr <mohammad_sakr_89@yahoo.com>
2021-08-25 00:49:30 +0200

    Roman to int

---
+++ 37_romanToInt/index.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+function romanToInt(s) {
+  const map = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let result = 0;
+
+  for (i = 0; i < s.length; i++) {
+    const cur = map[s[i]];
+    const next = map[s[i + 1]];
+
+    if (cur < next) {
+      result += next - cur; // IV -> 5 - 1 = 4
+      i++;
+    } else {
+      result += cur;
+    }
+  }
+
+  return result;
+}
+
+module.exports = romanToInt;
