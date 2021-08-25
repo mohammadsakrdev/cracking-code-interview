0e439b39d8482f5aca7ecebe6426962f9d7d5062
Mohammad Sakr <mohammad_sakr_89@yahoo.com>
2021-08-25 15:06:21 +0200

    58_canFinishCourseSchedule

---
+++ 58_canFinishCourseSchedule/index.js
@@ -0,0 +1,46 @@
+// There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1.
+// You are given an array prerequisites where // prerequisites[i] = [ai, bi] indicates that you must take course bi first
+// if you want to take course ai
+
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+function canFinish(numCourses, prerequisites) {
+  var edges = Array(numCourses)
+    .fill(0)
+    .map((_) => Array(numCourses).fill(0));
+  var incoming = Array(numCourses).fill(0);
+  var len = prerequisites.length;
+  var post = 0;
+  var prev = 0;
+  var queue = [];
+  var num = 0;
+  var count = 0;
+
+  for (var i = 0; i < len; i++) {
+    prev = prerequisites[i][1];
+    post = prerequisites[i][0];
+    if (edges[prev][post] === 0) {
+      incoming[post]++;
+      edges[prev][post] = 1;
+    }
+  }
+
+  for (var j = 0; j < numCourses; j++) {
+    if (incoming[j] === 0) queue.push(j);
+  }
+
+  while (queue.length) {
+    count++;
+    num = queue.pop();
+    for (var k = 0; k < numCourses; k++) {
+      if (edges[num][k] === 1 && --incoming[k] === 0) queue.push(k);
+    }
+  }
+
+  return count === numCourses;
+}
+
+module.exports = canFinish;
